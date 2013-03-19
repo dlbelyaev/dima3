14beb8b2477fd7adda3d766ebef0f57473991837
dlbelyaev <d.l.belyaev@gmail.com>
2013-03-19 16:05:01 +0400

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dima3::Application.config.secret_token = 'b43cd817b522bb80b7816822e44581731dfd9cd7370b75395c4ba9e63fccc0c29730ca9f49254ec7aa9e35ba7afdcc1e3166ab6c5ebcebe8e974cbd70ade2537'
